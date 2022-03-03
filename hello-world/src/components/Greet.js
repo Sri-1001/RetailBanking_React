fef160dcb59f433853ec010f4326f54dcaebc32a
Tanu-2505 <100665797+Tanu-2505@users.noreply.github.com>
2022-03-03 12:11:21 +0530

    Delete hello-world directory

--- hello-world/src/components/Greet.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-//function Greets()
-//{
-    //return <h1>hello anusha</h1>
-//}
-// arrow function
- export const Greets=()=><h1>hello vishwas</h1>
-//export default Greets;
-//name component
