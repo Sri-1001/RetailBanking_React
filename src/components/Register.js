c7d82d38dd2408fb8b3993d7b0fcc2601c938fbe
Tanu-2505 <tanu19223.it@rmkec.ac.in>
2022-03-08 13:16:56 +0530

    first commit

--- src/components/Register.js
+++ src/components/Register.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import _ from 'lodash';
 import { Form, Button } from 'react-bootstrap';
+import { registerNewUser } from '../actions/auth';
+import { resetErrors } from '../actions/errors';
 import { validateFields } from '../utils/common';
 import { Link } from 'react-router-dom';
 
@@ -15,6 +18,16 @@ class Register extends React.Component {
     errorMsg: '',
     isSubmitted: false
   };
+
+  componentDidUpdate(prevProps) {
+    if (!_.isEqual(prevProps.errors, this.props.errors)) {
+      this.setState({ errorMsg: this.props.errors });
+    }
+  }
+
+  componentWillUnmount() {
+    this.props.dispatch(resetErrors());
+  }
 
   registerUser = (event) => {
     event.preventDefault();
@@ -44,6 +57,16 @@ class Register extends React.Component {
         });
       } else {
         this.setState({ isSubmitted: true });
+        this.props
+          .dispatch(registerNewUser({ first_name, last_name, email, password }))
+          .then((response) => {
+            if (response.success) {
+              this.setState({
+                successMsg: 'User registered successfully.',
+                errorMsg: ''
+              });
+            }
+          });
       }
     }
   };
@@ -56,7 +79,16 @@ class Register extends React.Component {
   };
 
   render() {
-    const { errorMsg, successMsg, isSubmitted } = this.state;
+    const {
+      first_name,
+      last_name,
+      email,
+      password,
+      cpassword,
+      errorMsg,
+      successMsg,
+      isSubmitted
+    } = this.state;
     return (
       <div className="login-page">
         <h2>Register User</h2>
@@ -76,6 +108,7 @@ class Register extends React.Component {
               <Form.Control
                 type="text"
                 name="first_name"
+                value={first_name}
                 placeholder="Enter first name"
                 onChange={this.handleInputChange}
               />
@@ -85,6 +118,7 @@ class Register extends React.Component {
               <Form.Control
                 type="text"
                 name="last_name"
+                value={last_name}
                 placeholder="Enter last name"
                 onChange={this.handleInputChange}
               />
@@ -94,6 +128,7 @@ class Register extends React.Component {
               <Form.Control
                 type="email"
                 name="email"
+                value={email}
                 placeholder="Enter email"
                 onChange={this.handleInputChange}
               />
@@ -103,6 +138,7 @@ class Register extends React.Component {
               <Form.Control
                 type="password"
                 name="password"
+                value={password}
                 placeholder="Enter password"
                 onChange={this.handleInputChange}
               />
@@ -112,6 +148,7 @@ class Register extends React.Component {
               <Form.Control
                 type="password"
                 name="cpassword"
+                value={cpassword}
                 placeholder="Enter confirm password"
                 onChange={this.handleInputChange}
               />
@@ -131,4 +168,8 @@ class Register extends React.Component {
   }
 }
 
-export default connect()(Register);
+const mapStateToProps = (state) => ({
+  errors: state.errors
+});
+
+export default connect(mapStateToProps)(Register);
